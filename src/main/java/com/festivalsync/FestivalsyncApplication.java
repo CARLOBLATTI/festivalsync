1012b97044ebeb337eba6b357e54ae707f54f717
Carlo Blatti <carlo.blatti@outlook.it>
2024-11-10 22:00:06 +0100

    First commit

---
+++ src/main/java/com/festivalsync/FestivalsyncApplication.java
@@ -0,0 +1,13 @@
+package com.festivalsync.festivalsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalsyncApplication.class, args);
+	}
+
+}
